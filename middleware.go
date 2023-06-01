717e93b72488ec37c3e70398febcb6ff257652c7
Shayne Meyer <shayne.meyer@gmail.com>
2023-06-01 07:17:39 -0700

    fixed incorrect call

--- middleware.go
+++ middleware.go
@@ -3,6 +3,6 @@ package rasant
 import "net/http"
 
 func (ras *Rasant) SessionLoad(next http.Handler) http.Handler {
-	ras.InfoLog.Panicln("SessionLoad called")
+	ras.InfoLog.Println("SessionLoad called")
 	return ras.Session.LoadAndSave(next)
 }
