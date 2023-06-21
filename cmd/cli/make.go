5713e4fa62f1ba575d32d4879d201ded56e8cf64
Shayne Meyer <shayne.meyer@gmail.com>
2023-06-20 19:37:31 -0700

    Added support for generating mail templates with the cli

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -100,7 +100,23 @@ func doMake(arg2, arg3 string) error {
 			exitGracefully(err)
 		}
 
-		case "session":
+	case "mail":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the mail template a name"))
+		}
+		htmlMail := ras.RootPath + "/mail/" + strings.ToLower(arg3) + ".html.tmpl"
+		plainMail := ras.RootPath + "/mail/" + strings.ToLower(arg3) + ".plain.tmpl"
+
+		err := copyFileFromTemplate("templates/mailer/mail.html.tmpl", htmlMail)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		err = copyFileFromTemplate("templates/mailer/mail.plain.tmpl", plainMail)
+		if err != nil {
+			exitGracefully(err)
+		}
+	case "session":
 			err := doSessionTable()
 			if err != nil {
 				exitGracefully(err)
