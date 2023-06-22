cbea9ad03fb39b7f5de67674be641306e6bdf80e
Shayne Meyer <shayne.meyer@gmail.com>
2023-06-22 06:45:11 -0700

    Added Server to rasant.go

--- rasant.go
+++ rasant.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/CloudyKit/jet/v6"
@@ -47,6 +48,14 @@ type Rasant struct {
 	Cache cache.Cache
 	Scheduler *cron.Cron
 	Mail mailer.Mail
+	Server Server
+}
+
+type Server struct {
+	ServerName string
+	Port string
+	Secure bool
+	URL string
 }
 
 type config struct {
@@ -149,6 +158,18 @@ func (ras *Rasant) New(rootPath string) error {
 			password: os.Getenv("REDIS_PASSWORD"),
 			prefix: os.Getenv("REDIS_PREFIX"),
 		},
+	}
+
+	secure := true 
+	if strings.ToLower(os.Getenv("SECURE")) == "false" {
+		secure = false
+	}
+
+	ras.Server = Server{
+		ServerName: os.Getenv("SERVER_NAME"),
+		Port: os.Getenv("PORT"),
+    Secure: secure,
+    URL: os.Getenv("APP_URL"),
 	}
 
 	// create session
