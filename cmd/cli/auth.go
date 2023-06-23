467b381b2b726eb2bc086cdbfc4b57f1f194885f
Shayne Meyer <shayne.meyer@gmail.com>
2023-06-23 06:08:02 -0700

    Added auth and token templates to cli

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -41,6 +41,11 @@ func doAuth() error {
 		exitGracefully(err)
 	}
 
+	err = copyFileFromTemplate("templates/data/remember_token.go.txt", ras.RootPath + "/data/remember_token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
 	// copy over middleware
 	err = copyFileFromTemplate("templates/middleware/auth.go.txt", ras.RootPath + "/middleware/auth.go")
 	if err != nil {
@@ -51,6 +56,42 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	err = copyFileFromTemplate("templates/middleware/remember.go.txt", ras.RootPath + "/middleware/remember.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/handlers/auth-handlers.go.txt", ras.RootPath + "/handlers/auth-handlers.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.html.tmpl", ras.RootPath + "/mail/password-reset.html.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.plain.tmpl", ras.RootPath + "/mail/password-reset.plain.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/login.jet", ras.RootPath + "/views/login.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/forgot.jet", ras.RootPath + "/views/forgot.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/reset-password.jet", ras.RootPath + "/views/reset-password.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
 
 	color.Yellow("  - users, tokens, and remember_tokens migrations created and executed")
 	color.Yellow("  - user and token models created")
